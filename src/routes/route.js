6d4a0a8c0ea964c5d955db5d2a8df51470b03ec9
pattamu <102967813+pattamu@users.noreply.github.com>
2022-04-20 23:28:09 +0530

    assignment for middleware 20-04-22

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const developerController = require("../controllers/developerController")
+const orderController = require("../controllers/orderController")
+const {headerCheck} = require("../middleware/middleware")
 
 //Developer-Batch route handlers
-router.post("/batches", developerController.createBatch)
-router.post("/developers", developerController.createDeveloper)
-router.get("/scholarship-developers", developerController.scholarship_developers)
-router.get("/developers", developerController.getDeveloper)
-
-router.post("/ObjectId_Checker", developerController.ObjectIdCheck)
+router.post("/users", headerCheck, orderController.createUser)
+router.post("/products", orderController.createProduct)
+router.post("/orders", headerCheck, orderController.createOrder)
 
 module.exports = router;
 
