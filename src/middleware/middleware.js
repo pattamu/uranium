6e96a4521a7e09eaab5f5a123e043c916f948b23
pattamu <102967813+pattamu@users.noreply.github.com>
2022-04-21 16:36:27 +0530

    updated code

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -2,9 +2,8 @@ const headerCheck = async (req, res, next) => {
     const header = req.headers.isfreeappuser || req.headers.isFreeAppUser //some client send header as it is and some with lowercase so thi checks both our conditions
     if(header){
         if(header === "true")
-        req['isFreeAppUser'] = Boolean(header)  //5,6,7 all are different ways to assign Boolean value
-        // req['isFreeAppUser'] = true //we can do 'req.isFreeAppUser = true' as well. same thing
-        // req['isFreeAppUser'] = JSON.parse(header)
+        req['isFreeAppUser'] = true //we can do 'req.isFreeAppUser = true' as well. same thing
+        // req['isFreeAppUser'] = JSON.parse(header) // This way also we can write line 5
         if(header === "false")
         req['isFreeAppUser'] = false //in [] notation the value is considered to be a variable that's why we write the 'isFreeAppUser' with single/double quote to make it a string 
     next()
