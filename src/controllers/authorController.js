35885fb8ae3a465612bdcc29454424f2310bd213
pattamu <102967813+pattamu@users.noreply.github.com>
2022-04-26 23:10:24 +0530

    createAuthor getBlog apis created

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -1,5 +1,6 @@
 const {author, blog} = require("../models/schemas")
 
+//createAuthor API by Sandeep
 const createAuthor = async (req,res) => {
     try{
         let data = req.body
@@ -12,7 +13,39 @@ const createAuthor = async (req,res) => {
     }
 }
 
+//createBlog API by Sandeep
+// const createBlogs = async (req,res) => {
+//     try{
+//         let data = req.body
+//         if(!await author.findById(req.body.authorId)) 
+//         return res.status(208).send({status: false, msg: "AuthorId is not valid"})
+//         if(await blog.exists(data)) 
+//         return res.status(208).send({status: false, msg: "Blog already present"})
+//         let created = await blog.create(data)
+//         res.status(201).send({status: true, data: created})
+//     }
+//     catch(err){
+//         console.log(err.message)
+//         res.status(500).send({status: false, msg: err.message})
+//     }
+// }
 
-module.exports = {createAuthor}
+//getBlog API by Sandeep
+const getBlogs = async (req,res) => {
+    try{
+        req.query.isDeleted = false
+        req.query.isPublished = true
+        let filter = await blog.find(req.query)
+        if(!filter.length)
+        return res.status(404).send({status: false, msg: "No such documents found"})
+        res.status(200).send({status: true, data: filter})
+    }
+    catch(err){
+        console.log(err.message)
+        res.status(500).send({status: false, msg: err.message})
+    }
+}
 
-// createBlogs, getBlogs, updateBlogs, deleteBlogs, deleteBlogsQP
+module.exports = {createAuthor, getBlogs}
+
+// updateBlogs, deleteBlogs, deleteBlogsQP
--- src/routes/route.js
+++ src/routes/route.js
@@ -5,7 +5,7 @@ const controller = require("../controllers/authorController")
 //Author-Blog route Hndlers
 router.post("/author", controller.createAuthor)
 // router.post("/blogs", controller.createBlogs)
-// router.get("/blogs", controller.getBlogs)
+router.get("/blogs", controller.getBlogs)
 // router.put("/blogs/:blogId", controller.updateBlogs)
 // router.delete("/blogs/:blogId", controller.deleteBlogs)
 // router.delete("/blogs?queryParams", controller.deleteBlogsQP)
