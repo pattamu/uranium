313082add3608ea436557ec7efc034a4db5c78f1
pattamu <102967813+pattamu@users.noreply.github.com>
2022-05-04 20:45:59 +0530

    index.js and package.json modification

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,8 +4,8 @@ const collegeController = require('../controller/collegeController')
 const {createIntern, getInters} = require('../controller/internController')
 
 //open-college-intern route Hndlers
-router.post("/functionup/colleges", collegeController)
-router.post("/functionup/interns", createIntern)
-router.get("/functionup/collegeDetails", getInters)
+router.post("/colleges", collegeController)
+router.post("/interns", createIntern)
+router.get("/collegeDetails", getInters)
 
 module.exports = router;
